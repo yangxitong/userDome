6379dd37062d95f24046f59c72a41ecf82d16c85
yangxitong <513933837@qq.com>
2016-04-15 16:20:00 +0800

    1

--- src/com/yxt/test/Cat.java
+++ src/com/yxt/test/Cat.java
@@ -1,8 +1,37 @@
 package com.yxt.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cat {
 	 
 	private int id;
+	
+	private String name;
+	
+	public Cat() {
+	}
+	
+	
+	public Cat(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Cat [id=" + id + ", name=" + name + "]";
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public int getId() {
 		return id;
@@ -11,9 +40,19 @@ public class Cat {
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	
 	public static void main(String[] args) {
 		System.out.println("123312");
 		System.out.println("23142");
+		List<Cat> cats = new ArrayList<>();
+		cats.add(0, new Cat(1,"Max"));
+		cats.add(1,new Cat(2, "Happy!"));
+		
+		for(Cat cat : cats) {
+			System.out.println(cat.getId()+"name:"+cat.getName());
+		}
+		
+		
 	}
 }
